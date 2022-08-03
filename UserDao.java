34649bcc65040965c6603c84aabd3441c2547593
Sarthak IT <sarthakjenait24@gmail.com>
2022-08-03 10:11:42 +0530

    Bug-444 fix

--- UserDao.java
+++ UserDao.java
@@ -1,7 +1,7 @@
 public static void main(String[] args) {
 		
 		//Arrays are type safe 
-		int[] numbers = {10,20,30};
+		int[] numbers = {10,20,30,40};
 		for(int number : numbers)
 		{	System.out.println(number);
 		}
